79f6234f4577d5fe4248a90c79db91c880bf04f8
DianaZepeda7 <139736190+DianaZepeda7@users.noreply.github.com>
2023-10-26 18:54:25 -0700

    Add Login, Home and Prestamos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/styles/login.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from "./components/home";
+import { Prestamos } from "./components/prestamos";
+import { Login } from  "./components/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/prestamos" element={<Prestamos />} />
+      </Routes>
+    </Router>
   );
 }
 
